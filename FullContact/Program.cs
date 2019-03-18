60f1c6ab732f023d38e46522720e9706344ae6e2
lunderot <matriz@live.se>
2019-03-18 17:09:05 +0100

    Added basic project with REST request

---
+++ FullContact/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RestSharp;
+using Nito.AsyncEx;
+using System.Threading.Tasks;
+
+namespace FullContact
+{
+    class Program
+    {
+        private static async Task<int> AsyncMain()
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("todos/{id}");
+            request.AddUrlSegment("id", "2");
+
+
+            client.ExecuteAsync(request, r =>
+            {
+                Console.WriteLine(r.Content);
+            });
+            var response = await client.ExecuteTaskAsync(request);
+
+            return 0;
+
+        }
+
+        static int Main(string[] args)
+        {
+            AsyncContext.Run(AsyncMain);
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
