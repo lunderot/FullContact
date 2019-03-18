e4acf3211e78002c085588de9c09b52975133cff
lunderot <matriz@live.se>
2019-03-18 20:27:46 +0100

    Added menu system

--- FullContact/Program.cs
+++ FullContact/Program.cs
@@ -8,20 +8,34 @@ namespace FullContact
 {
     class Program
     {
-        private static async Task<int> AsyncMain()
-        {
-            API fc = new API(Environment.GetEnvironmentVariable("API_KEY"));
-            FullContactPerson person = await fc.LookupPersonByEmailAsync("bill.gates@microsoft.com");
-
-            Console.WriteLine(person.contactInfo.fullName);
-            Console.WriteLine(person.likelihood);
-            return 0;
-        }
-
         static int Main(string[] args)
         {
-            AsyncContext.Run(AsyncMain);
-            Console.ReadKey();
+            API fc = new API(Environment.GetEnvironmentVariable("API_KEY"));
+            do
+            {
+                Console.Write("Please enter an email (or enter nothing to quit): ");
+                var email = Console.ReadLine();
+                if(email == "")
+                    break;
+                var person = AsyncContext.Run<FullContactPerson>(async () => await fc.LookupPersonByEmailAsync(email));
+
+                Console.WriteLine("Full name: " + person.contactInfo.fullName);
+                Console.WriteLine("Likelihood: " + person.likelihood);
+
+                Console.WriteLine("Websites: ");
+                foreach (var website in person.contactInfo.websites)
+                {
+                    Console.WriteLine("\t" + website.url);
+                }
+
+                Console.WriteLine("Social media: ");
+                foreach(var profile in person.socialProfiles)
+                {
+                    Console.WriteLine("\t" + profile.typeName + ": " + profile.url);
+                }
+                Console.WriteLine();
+            }
+            while (true);
             return 0;
         }
     }
